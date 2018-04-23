c2505c61faefd7b54f2e876178c450cf1b5032fe
berjk <berjk@users.noreply.github.com>
2018-04-23 18:14:04 +0300

    Version update to v1.0.8

--- ERASynth/ERASynth.ino
+++ ERASynth/ERASynth.ino
@@ -127,9 +127,9 @@ boolean isPulseRising			= false;
 boolean is_pulse_changed		= false;
 
 String ESP8266FirmwareVersion_Str	= "";
-String embeddedVersion_Str			= "v1.0.7";
-String cmdString					= "";
-String cmd1String					= "";
+String embeddedVersion_Str		= "v1.0.8";
+String cmdString			= "";
+String cmd1String			= "";
 String frequency_Str				= "";
 String amplitude_Str				= "";
 String startFrequency_Str			= "";
